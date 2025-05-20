f2283c4f8e053f7e9b104941b1000e905849aed6
Christian Haitian <christianhaitian0@gmail.com>
2025-05-20 03:47:31 -0400

    Fix license file copying for ppsspp build

--- build_ppssppsa.sh
+++ build_ppssppsa.sh
@@ -15,7 +15,7 @@ sudo cp ppsspp/ppssppkeydemon.py.rgb10 Arkbuild/usr/local/bin/ppssppkeydemon.py
 sudo cp -R ppsspp/configs/backupforromsfolder/ Arkbuild/opt/ppsspp/
 sudo cp ppsspp/controls.ini.rgb10 Arkbuild/opt/ppsspp/backupforromsfolder/ppsspp/PSP/SYSTEM/controls.ini
 sudo cp ppsspp/ppsspp.ini.rgb10 Arkbuild/opt/ppsspp/backupforromsfolder/ppsspp/PSP/SYSTEM/ppsspp.ini
-sudo cp -a Arkbuild/home/ark/rk3326_core_builds/ppsspp/LICENSE.TXT/ Arkbuild/opt/ppsspp/
+sudo cp -a Arkbuild/home/ark/rk3326_core_builds/ppsspp/LICENSE.TXT Arkbuild/opt/ppsspp/
 sudo cp -a Arkbuild/home/ark/rk3326_core_builds/ppsspp/build/PPSSPPSDL Arkbuild/opt/ppsspp/
 sudo chroot Arkbuild/ bash -c "chown -R ark:ark /opt/"
 sudo chmod 777 Arkbuild/opt/ppsspp/PPSSPPSDL
